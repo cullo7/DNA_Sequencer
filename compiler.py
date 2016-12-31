07df39c4925bb738d0ce45e65e22cbba1b929b83
cullo7 <culloaiden3@gmail.com>
2016-12-31 16:15:04 -0500

    added makefile and cleaned up style

--- compiler.py
+++ compiler.py
@@ -1,18 +1,21 @@
 """
-    Compiler for a DNA duplex to calculate the dissociation and association melting temperature 
+    Compiler for a DNA duplex to calculate the dissociation and
+    association melting temperature
 """
 
-import sys
 import math
+import sys
 from subprocess import call
-from energy import get_initiation_energy as i_energy
+
 from energy import get_end_energy as e_energy
+from energy import get_initiation_energy as i_energy
 from energy import get_nearest_neighbor_energy as nn_energy
 from energy import get_nearest_neighbor_mismatch_energy as nn_mm_energy
 from energy import get_symmetry_g as get_sym
-#from graphic import visualize_genome as visualize
 
-# string that will hold the dna sequence 
+# from graphic import visualize_genome as visualize
+
+# string that will hold the dna sequence
 prime_5 = ""
 prime_3 = ""
 # molarity of duplex and each strand alone
@@ -21,16 +24,22 @@ molarity_p5 = 0
 molarity_p3 = 0
 
 """
-  Calculate melting temperature of dissociation based on nearest neighbor method
+    Calculate melting temperature of dissociation based on nearest i
+    neighbor method
 """
+
+
 def find_melting_temperature():
     global prime_5, prime_3
-    #iterates over shortest strand
+    # iterates over shortest strand
     length = len(prime_5) if len(prime_5) < len(prime_3) else len(prime_3)
-    #create tuple that will hold [enthalpy, entropy, gibb's free energy]
-    sequence_data = [0.0,0.0,0.0] 
+    # initial and terminal base pairs
+    initial = prime_3[0]+prime_5[0]
+    terminal = prime_3[length-1]+prime_5[length-1]
+    # create tuple that will hold [enthalpy, entropy, gibb's free energy]
+    sequence_data = [0.0, 0.0, 0.0]
     # calculate energy values of first pair ignoring nearest neighbor
-    sequence_data = add(sequence_data, i_energy(prime_3[0]+prime_5[0],prime_3[length-1]+prime_5[length-1]))
+    add(sequence_data, i_energy(initial, terminal))
 
     symmetrical = True
 
@@ -39,58 +48,69 @@ def find_melting_temperature():
         if(prime_3[x]+prime_5[x] != prime_5[length-1-x]+prime_3[length-1-x]):
             symmetrical = False
         # calculates base-pair energy based on nearest_neighbor
-        # if base pair is complementary then we find its nearest neighbor energy
-        if (not is_complement(prime_5[x], prime_3[x]) and x == 0) or (not is_complement(prime_5[x+1], prime_3[x+1]) and x+1 == length):
+        # find complementary base pair nearest neighbor energy
+        if (not is_complement(prime_5[x], prime_3[x]) and x == 0) or \
+                (not is_complement(prime_5[x+1], prime_3[x+1])
+                    and x+1 == length):
             # We cannot evaluate a mismatch on the end of a sequence yet
             continue
-        elif is_complement(prime_5[x+1], prime_3[x+1]) and is_complement(prime_3[x], prime_5[x]):
-            #print(prime_3[x]+prime_3[x+1] +", "+ prime_5[x]+prime_5[x+1])
-            sequence_data = add(sequence_data, nn_energy(prime_3[x:x+2],prime_5[x:x+2]))
-        # if base pair isn't a complement we look for its mismatch value as long as it is not the 
-        # end of the sequence
+        elif is_complement(prime_5[x+1], prime_3[x+1]) and \
+                is_complement(prime_3[x], prime_5[x]):
+            # print(prime_3[x]+prime_3[x+1] +", "+ prime_5[x]+prime_5[x+1])
+            add(sequence_data, nn_energy(prime_3[x:x+2], prime_5[x:x+2]))
+        # look for base pair inverse mismatch value
         else:
-            #print(prime_3[x]+prime_3[x+1] +", "+ prime_5[x]+prime_5[x+1])
-            sequence_data = add(sequence_data, nn_mm_energy(prime_3[x:x+2],prime_5[x:x+2]))
+            # print(prime_3[x]+prime_3[x+1] +", "+ prime_5[x]+prime_5[x+1])
+            add(sequence_data, nn_mm_energy(prime_3[x:x+2], prime_5[x:x+2]))
 
-    # calculate free energy, entropy and enthalpy of last pair ignoring nearest neighbor
-    sequence_data = add(sequence_data, e_energy(prime_3[0]+prime_5[0],prime_3[length-1]+prime_5[length-1]))
-    if symmetrical:
-        sequence_data = add(sequence_data, get_sym())
-    
+    # checks for terminal 5'-> 3' T-A base pair
+    add(sequence_data, e_energy(initial, terminal))
+    # if sequence is symmetrical, add symmetry value
+    add(sequence_data, get_sym(symmetrical))
+
     # Account for Other factors that will affect temperature/gibbs energy
 
     """
         Calculate temperature from Gibbs Energy:
         [A], [B] - molarity of dissociated single strand
         [AB] - molarity of associated double strand
-        If no additional nucleic acids are present, then [A], [B], and [AB] will be equal, and equal to half the 
-        initial concentration of double-stranded nucleic acid, [AB]initial. This gives an expression for the melting 
-        point of a nucleic acid duplex of
+        If no additional nucleic acids are present, then [A], [B], and [AB]
+        will be equal, and equal to half the initial concentration of
+        double-stranded nucleic acid, [AB]initial. This gives an expression for
+        the melting point of a nucleic acid duplex of
     """
     AB_mol = 1
-    Mg_mol = .5
+    # Mg_mol = .5
     R = 8.3144598
-    temperature = -(sequence_data[2])/(R*math.log(AB_mol/2,math.exp(1)))
+    temperature = -(sequence_data[2])/(R*math.log(AB_mol/2, math.exp(1)))
 
     """
     temperature calculation with salt
     a = math.pow(3.92,-5)
-    b = math.pow(-9.11,-6) 
+    b = math.pow(-9.11,-6)
     c = math.pow(6.26,-5)
     d = math.pow(1.42,-5)
     e = math.pow(-4.82,-4)
     f = math.pow(5.25,-4)
     g = math.pow(8.31,-5)
-    temperature_salt = (1/temperature) + a + b*math.log(Mg_mol, math.exp(1)) + (f * (c+d*math.log(Mg_mol,math.exp(1))))+ ((1/(2*(length-1)))*(e + (f*math.log(Mg_mol, math.exp(1))) + (g*math.pow(math.log(Mg_mol, math.exp(1)), 2))))
+    temperature_salt = (1/temperature) + a + b*math.log(Mg_mol, math.exp(1)) +/
+        (f * (c+d*math.log(Mg_mol,math.exp(1))))+ ((1/(2*(length-1)))*(e +\
+            (f*math.log(Mg_mol, math.exp(1))) +\
+                (g*math.pow(math.log(Mg_mol, math.exp(1)), 2))))
     """
- 
+
     return sequence_data+[length]+[temperature]
+
 
 """
    string from text file and creates DNA sequence for 5'->3' and 3'->5' strand
 """
+
+
 def parse_genome(number):
     global prime_5, prime_3
+    prime_5 = ""
+    prime_3 = ""
     with open("dna_samples/dna_sample_"+str(number)+".txt") as f:
         seq = f.read()
     seq = seq.split()
@@ -98,11 +118,12 @@ def parse_genome(number):
     for x in range(len(seq)):
         if seq[x] != '/n' and seq[x] != ' ':
             if even:
-                prime_3+=seq[x]
+                prime_3 += seq[x]
                 even = False
             else:
-                prime_5+=seq[x]
+                prime_5 += seq[x]
                 even = True
+
 
 # Show help menu
 def help():
@@ -113,18 +134,21 @@ def help():
     print("show: Show sequence and expected result for that sequence")
     print("details: Explains experimental conditions and procedure\n")
 
-# runs program on dna sequence and compares it to expected value from catalog.txt file
+
+# runs program on dna sequence and compares it to expected value
 def test(number):
     parse_genome(number)
     data = find_melting_temperature()
-    print("Length: "+ str(data[3]))
-    print("Gibb's free energy: "+ str(data[2]))
-    print("Entropy; "+ str(data[1]))
-    print("Enthalpy: "+ str(data[0]))
-    print("Temperature: "+ str(data[4]))
+    print("Length: " + str(round(data[3], 2)))
+    print("Gibb's free energy: " + str(round(data[2], 2)))
+    print("Entropy; " + str(round(data[1], 2)))
+    print("Enthalpy: " + str(round(data[0], 2)))
+    print("Temperature: " + str(round(data[4], 2)))
     # print correct result
     show_results(number)
 
+
+# details about the program
 def details():
     print("Parameters:")
     print("pH: 7")
@@ -140,6 +164,7 @@ def details():
     print("dissociates 1/2 of its base pairs from the Gibb's energy")
     print("contribution of each base pair bond.\n")
 
+
 # prints out a dna sequence from a sample file
 def show(number):
     print("Sequence\n")
@@ -148,28 +173,35 @@ def show(number):
         print(f.read())
     # printing expected output for DNA sample
     show_results(number)
-    
+
+
 # show expected results on file
 def show_results(num):
     print("Expected Output:")
     start = ((int(num)-1)*7)+1
-    end =  start+5
+    end = start+5
     s = str(start)+","+str(end)+"p"
-    call(["sed", "-n",s , "dna_samples/catalog.txt"])
+    call(["sed", "-n", s, "dna_samples/catalog.txt"])
+
 
 # show all sample sequences
 def show_all(n):
     for x in range(n):
         show(x+1)
 
+
 # test all samples
 def test_all(n):
     for x in range(n):
             test(x+1)
-    
+
+
 # add tuples of size 3
 def add(t1, t2):
-    return [t1[0]+t2[0], t1[1]+t2[1], t1[2]+t2[2]]
+    t1[0] += t2[0]
+    t1[1] += t2[1]
+    t1[2] += t2[2]
+
 
 # test if bases are complements
 def is_complement(b1, b2):
@@ -179,21 +211,22 @@ def is_complement(b1, b2):
         return True
     return False
 
+
 if __name__ == '__main__':
 
-    details() 
+    details()
 
-    # nnumber of samples
+    # number of samples
     with open("dna_samples/catalog.txt") as f:
         samples = int(sum(1 for _ in f)/7)
 
     # Command prompt loop
     while True:
-        command = (input("[DNA_compiler]: ")).strip() 
-        if  command == "help":
+        command = (input("[DNA_compiler]: ")).strip()
+        if command == "help":
             help()
         elif command == "test":
-            file_name = (input("select a number between 1 and 25, inclusive: ")).strip()
+            file_name = (input("select a number from 1 and 25: ")).strip()
             if file_name.isdigit():
                 test(file_name)
             elif file_name == "all":
@@ -202,11 +235,11 @@ if __name__ == '__main__':
                 print("Invalid input: an integer between 1 and 25 required")
                 continue
         elif command == "show":
-            file_name = (input("select a number between 1 and 25, inclusive: ")).strip()
+            file_name = (input("select a number from 1 and 25: ")).strip()
             if file_name.isdigit():
                 show(file_name)
             elif file_name == "all":
-               show_all(samples)
+                show_all(samples)
             else:
                 print("Invalid input: an integer between 1 and 25 required")
                 continue
--- energy.py
+++ energy.py
@@ -1,171 +1,198 @@
 """
-    All possible base pairs delta G energy from https://en.wikipedia.org/wiki/Nucleic_acid_thermodynamics
+    All possible base pairs delta G energy from
+    https://en.wikipedia.org/wiki/Nucleic_acid_thermodynamics
 """
 base_stability_energy = {
-    "TA" : -0.12,
-    "TG" : -0.78,
-    "CA" : -0.78,
-    "CG" : -1.44,
-    "AG" : -1.29,
-    "CT" : -1.29,
-    "AA" : -1.04,
-    "TT" : -1.04,
-    "AT" : -1.27,
-    "GA" : -1.66,
-    "TC" : -1.66,
-    "CC" : -1.97,
-    "GG" : -1.97,
-    "AC" : -2.04,
-    "GT" : -2.04,
-    "GC" : -2.70
+    "TA": -0.12,
+    "TG": -0.78,
+    "CA": -0.78,
+    "CG": -1.44,
+    "AG": -1.29,
+    "CT": -1.29,
+    "AA": -1.04,
+    "TT": -1.04,
+    "AT": -1.27,
+    "GA": -1.66,
+    "TC": -1.66,
+    "CC": -1.97,
+    "GG": -1.97,
+    "AC": -2.04,
+    "GT": -2.04,
+    "GC": -2.70
 }
 
 """
-    Gibb's free energy from each adjacent base pair using the nearest neighbor 
+    Gibb's free energy from each adjacent base pair using the nearest neighbor
     approach from https://en.wikipedia.org/wiki/Nucleic_acid_thermodynamics
 """
 nearest_neighbor_energy = {
-    "AATT" : [-8.4, -23.6, -1.02],
-    "ATTA" : [-6.5, -18.8, -0.73],
-    "TAAT" : [-6.3, -18.5, -0.60],
-    "CAGT" : [-7.4, -19.3, -1.38],
-    "GTCA" : [-8.6, -23.0, -1.43],
-    "CTGA" : [-6.1, -16.1, -1.16],
-    "GACT" : [-7.7, -20.3, -1.46],
-    "CGGC" : [-10.1, -25.5, -2.09],
-    "GCCG" : [-11.1, -28.4, -2.28],
-    "GGCC" : [-6.7, -15.6, -1.77],
-    "AT" : [0.0, -9.0, 2.8],
-    "CG" : [0.0, -5.9, 1.82],
-    "SYM" : [0.0, -1.4, 0.4],
-    "TA" : [0.4, 0, 0.4]
+    "AATT": [-8.4, -23.6, -1.02],
+    "ATTA": [-6.5, -18.8, -0.73],
+    "TAAT": [-6.3, -18.5, -0.60],
+    "CAGT": [-7.4, -19.3, -1.38],
+    "GTCA": [-8.6, -23.0, -1.43],
+    "CTGA": [-6.1, -16.1, -1.16],
+    "GACT": [-7.7, -20.3, -1.46],
+    "CGGC": [-10.1, -25.5, -2.09],
+    "GCCG": [-11.1, -28.4, -2.28],
+    "GGCC": [-6.7, -15.6, -1.77],
+    "AT": [0.0, -9.0, 2.8],
+    "CG": [0.0, -5.9, 1.82],
+    "SYM": [0.0, -1.4, 0.4],
+    "TA": [0.4, 0, 0.4]
 }
 
 """
-    Gibb's free energy from each adjacent mismatch base pair using the nearest neighbor 
-    approach with trimer surrounding mismatch.
+    Gibb's free energy from each adjacent mismatch base pair using the nearest
+    neighbor approach with trimer surrounding mismatch.
     TODO: C-A, C-C, G-G, A-A, T-T, G-A, G-T
 """
 nearest_neighbor_mismatch_energy = {
-    #C-T 
-    "ACTT" : [0.7, 0.2, 0.64],
-    "ATTC" : [-1.2, -6.2, 0.73],
-    "CCGT" : [-0.8, -4.5, 0.62],
-    "CTGC" : [-1.5, -6.1, 0.40],
-    "GCCT" : [2.3, 5.4, 0.62],
-    "GTCC" : [5.2, 13.5, 0.98],
-    "TCAT" : [1.2, -20.3, 0.97],
-    "TTAC" : [1.0, 0.7, 0.75],
+    # C-T
+    "ACTT": [0.7, 0.2, 0.64],
+    "ATTC": [-1.2, -6.2, 0.73],
+    "CCGT": [-0.8, -4.5, 0.62],
+    "CTGC": [-1.5, -6.1, 0.40],
+    "GCCT": [2.3, 5.4, 0.62],
+    "GTCC": [5.2, 13.5, 0.98],
+    "TCAT": [1.2, -20.3, 0.97],
+    "TTAC": [1.0, 0.7, 0.75],
 
-    #C-A
-    "AATC" : [2.3, 4.6, 0.88],
-    "ACTA" : [5.3, 14.6, 0.77],
-    "CAGC" : [1.9, 3.7, 0.75],
-    "CCGA" : [0.6, -0.6, 0.79],
-    "GACC" : [5.2, 14.2, 0.81],
-    "GCCA" : [-0.7, -3.8, 0.47],
-    "TAAC" : [3.4, 8.0, 0.92],
-    "TCAA" : [7.6, 20.2, 1.33],
+    # C-A
+    "AATC": [2.3, 4.6, 0.88],
+    "ACTA": [5.3, 14.6, 0.77],
+    "CAGC": [1.9, 3.7, 0.75],
+    "CCGA": [0.6, -0.6, 0.79],
+    "GACC": [5.2, 14.2, 0.81],
+    "GCCA": [-0.7, -3.8, 0.47],
+    "TAAC": [3.4, 8.0, 0.92],
+    "TCAA": [7.6, 20.2, 1.33],
 
-    #G-A
-    "AATG" : [-0.6, -2.3, 0.14],
-    "AGTA" : [-0.7, -2.3, 0.02],
-    "CAGG" : [-0.7, -2.3, 0.03],
-    "CGGA" : [-4.0, -13.2, 0.11],
-    "GACG" : [-0.6, -1.0, -0.25],
-    "GGCA" : [0.5, 3.2, -0.52],
-    "TAAG" : [0.7, 0.7, 0.42],
-    "TGAA" : [3.0, 7.4, 0.74],
+    # G-A
+    "AATG": [-0.6, -2.3, 0.14],
+    "AGTA": [-0.7, -2.3, 0.02],
+    "CAGG": [-0.7, -2.3, 0.03],
+    "CGGA": [-4.0, -13.2, 0.11],
+    "GACG": [-0.6, -1.0, -0.25],
+    "GGCA": [0.5, 3.2, -0.52],
+    "TAAG": [0.7, 0.7, 0.42],
+    "TGAA": [3.0, 7.4, 0.74],
 
-    #G-T
-    "AGTT" : [1.0, 0.9, 0.71],
-    "ATTG" : [-2.5, -8.3, 0.07],
-    "CGGT" : [-4.1, -11.7, -0.47],
-    "CTGG" : [-2.8, -8.0, -0.32],
-    "GGCT" : [3.3, 10.4, 0.08],
-    "GTCG" : [-4.4, -12.3, -0.59],
-    "TGAT" : [-0.1, -1.7, 0.43],
-    "TTAG" : [-1.3, -5.3, 0.34],
+    # G-T
+    "AGTT": [1.0, 0.9, 0.71],
+    "ATTG": [-2.5, -8.3, 0.07],
+    "CGGT": [-4.1, -11.7, -0.47],
+    "CTGG": [-2.8, -8.0, -0.32],
+    "GGCT": [3.3, 10.4, 0.08],
+    "GTCG": [-4.4, -12.3, -0.59],
+    "TGAT": [-0.1, -1.7, 0.43],
+    "TTAG": [-1.3, -5.3, 0.34],
 
-    #C-C
-    "ACTC" : [0.0, -4.4, 1.33],
-    "CCGC" : [-1.5, -7.2, 0.7],
-    "GCCC" : [3.6, 8.9, 0.79],
-    "TCAC" : [6.1, 16.4, 1.05],
+    # C-C
+    "ACTC": [0.0, -4.4, 1.33],
+    "CCGC": [-1.5, -7.2, 0.7],
+    "GCCC": [3.6, 8.9, 0.79],
+    "TCAC": [6.1, 16.4, 1.05],
 
-    #A-A
-    "AATA" : [1.2, 1.7, 0.61],
-    "CAGA" : [-0.9, -4.2, 0.43],
-    "GACA" : [-2.9, -9.8, 0.17],
-    "TAAA" : [4.7, 12.9, 0.69],
+    # A-A
+    "AATA": [1.2, 1.7, 0.61],
+    "CAGA": [-0.9, -4.2, 0.43],
+    "GACA": [-2.9, -9.8, 0.17],
+    "TAAA": [4.7, 12.9, 0.69],
 
-    #G-G
-    "AGTG" : [-3.1, -9.5, -0.13],
-    "CGGG" : [-4.9, -15.3, -0.11],
-    "GGCG" : [-6.0, -15.8, -1.11],
-    "TGAG" : [1.6, 3.6, 0.44],
+    # G-G
+    "AGTG": [-3.1, -9.5, -0.13],
+    "CGGG": [-4.9, -15.3, -0.11],
+    "GGCG": [-6.0, -15.8, -1.11],
+    "TGAG": [1.6, 3.6, 0.44],
 
-    #T-T
-    "ATTT" : [-2.7, -10.8, 0.69],
-    "CTGT" : [-5.0, -15.8, -0.12],
-    "GTCT" : [-2.2, -8.4, 0.45],
-    "TTAT" : [0.2, -1.5, 0.68],
+    # T-T
+    "ATTT": [-2.7, -10.8, 0.69],
+    "CTGT": [-5.0, -15.8, -0.12],
+    "GTCT": [-2.2, -8.4, 0.45],
+    "TTAT": [0.2, -1.5, 0.68],
 }
+
 
 # Find energy contribution of head and tail base pairs in DNA sequence
 def get_initiation_energy(init_pair, term_pair):
     total = [0.0, 0.0, 0.0]
-    # if either the initial or terminal base pair is C-G or G-C we add thes energy 
-    print("initial energy: "+init_pair+" "+term_pair)
-    if (init_pair == "GC" or term_pair == "CG") or (init_pair == "CG" or term_pair == "GC"):
+    """
+        if either the initial or terminal base pair is C-G or G-C
+        we add a specified energy
+    """
+    print("initial energy: " + init_pair + " " + term_pair)
+    if (init_pair == "GC" or term_pair == "CG") or \
+            (init_pair == "CG" or term_pair == "GC"):
         print("Adding GC val")
         total = add(total, nearest_neighbor_energy["CG"])
-    # If intial and terminal base pair are A-T or T-A then we add a certain enrgy value
-    if (init_pair == "AT" or term_pair == "TA") and (init_pair == "TA" or term_pair == "AT"):
+    """
+        If intial and terminal base pair are A-T or T-A then
+        we add a specified enrgy value
+    """
+    if (init_pair == "AT" or term_pair == "TA") and \
+            (init_pair == "TA" or term_pair == "AT"):
         print("Adding AT val")
         total = add(total, nearest_neighbor_energy["AT"])
     print(total)
     return total
 
+
 # get value for symmetry when sequence is symmetrical
-def get_symmetry_g():
-    return nearest_neighbor_energy["SYM"]
+def get_symmetry_g(sym):
+    if(sym):
+        return nearest_neighbor_energy["SYM"]
+    else:
+        return [0, 0, 0]
+
 
 # evaluate energy contribution of the terminal pair of a sequence
 def get_end_energy(init_pair, term_pair):
-    total = [0.0,0.0,0.0];
+    total = [0.0, 0.0, 0.0]
     # If terminal pair is 3' -> 5' == A -> T
     if term_pair == "AT":
         print("Adding A-T energy")
-        total=add(total,nearest_neighbor_energy["AT"])
+        total = add(total, nearest_neighbor_energy["AT"])
     return total
 
-# Find energy contribution of inner adjacent dimer (AT/TA) from nearest neigbor model
+
+"""
+    Find energy contribution of inner adjacent dimer (AT/TA)
+    from nearest neigbor model
+"""
+
+
 def get_nearest_neighbor_energy(pair_one, pair_two):
     print("nn")
-    if pair_one+pair_two in nearest_neighbor_energy:
-        #print(nearest_neighbor_energy[pair_one+pair_two][2])
-        return nearest_neighbor_energy[pair_one+pair_two]
-    # if base pair sequence isn't found we can look for its reverse which will have the same energy (TC/AG == GA/CT)
-    elif pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0] in nearest_neighbor_energy:
-        #print(nearest_neighbor_energy[pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0]][2])
-        return nearest_neighbor_energy[pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0]]
+    regular = pair_one + pair_two
+    inverse = pair_two[1] + pair_two[0] + pair_one[1] + pair_one[0]
+    if regular in nearest_neighbor_energy:
+        # print(nearest_neighbor_energy[pair_one+pair_two][2])
+        return nearest_neighbor_energy[regular]
+    # if sequence isn't found we use its inverse (e. g. TC/AG == GA/CT)
+    elif inverse in nearest_neighbor_energy:
+        # print(nearest_neighbor_energy[inverse[2])
+        return nearest_neighbor_energy[inverse]
     else:
-        print("Nearest neighbor sequence: "+str(pair_one+pair_two)+ "not found")
+        print("Nearest neighbor sequence: " + str(regular) + "not found")
 
-# # Find energy contribution of inner base pair mismatch from trimer(ATA/TAT) with the mismatch as its middle pair
+
+# Energy contribution from mismatch trimer(e.g. ATA/TTT)
 def get_nearest_neighbor_mismatch_energy(pair_one, pair_two):
     print("mm")
-    if pair_one+pair_two in nearest_neighbor_mismatch_energy:
-        #print(nearest_neighbor_mismatch_energy[pair_one+pair_two][2])
-        return nearest_neighbor_mismatch_energy[pair_one+pair_two]
-    # if base pair sequence isn't found we can look for its reverse which will have the same energy (TCT/AGC == CGA/TCT)
-    elif pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0] in nearest_neighbor_mismatch_energy:
-        #print(nearest_neighbor_mismatch_energy[pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0]][2])
-        return nearest_neighbor_mismatch_energy[pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0]]
+    regular = pair_one + pair_two
+    inverse = pair_two[1] + pair_two[0] + pair_one[1] + pair_one[0]
+    if regular in nearest_neighbor_mismatch_energy:
+        # print(nearest_neighbor_mismatch_energy[pair_one+pair_two][2])
+        return nearest_neighbor_mismatch_energy[regular]
+    # look for equivalent inverse sequence (e.g. TCT/AGC == CGA/TCT)
+    elif inverse in nearest_neighbor_mismatch_energy:
+        # print(nearest_neighbor_mismatch_energy[pair_two[1]+pair_two[0]+pair_one[1]+pair_one[0]][2])
+        return nearest_neighbor_mismatch_energy[inverse]
     else:
-        print("Mismatch sequence: "+str(pair_one+pair_two)+ "not found")
+        print("Mismatch sequence: " + str(regular) + "not found")
+
 
 # adding trimers -- tuples
 def add(t1, t2):
-    return [t1[0]+t2[0], t1[1]+t2[1], t1[2]+t2[2]]
+    return [t1[0] + t2[0], t1[1] + t2[1], t1[2] + t2[2]]
